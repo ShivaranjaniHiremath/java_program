beec7017a58701c48f15cb444856a7aeb1f0a215
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 09:01:30 +0530

    Loginot

---
+++ Loginot.java
@@ -0,0 +1,9 @@
+class Loginot{
+		public static void main(String args[]){
+			System.out.println("illustrating logical not operator");
+			
+			true=!false;
+			boolean flag=false;
+			System.out.println("the value of flag:" + flag);
+		}
+}
