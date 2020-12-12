82a37f7961a112cea21a3ea6cd05ccc233999764
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 09:02:34 +0530

    RelationalOperators

---
+++ RelationalOperators.java
@@ -0,0 +1,18 @@
+class RelationalOperators {
+
+	public static void main(String args[]) {
+
+
+      System.out.println("illustrating relational operators");
+	  
+      int a = 10;
+      int b = 20;
+
+      System.out.println("a == b = " + (a == b) );
+      System.out.println("a != b = " + (a != b) );
+      System.out.println("a > b = " + (a > b) );
+      System.out.println("a < b = " + (a < b) );
+      System.out.println("b >= a = " + (b >= a) );
+      System.out.println("b <= a = " + (b <= a) );
+   }
+}
