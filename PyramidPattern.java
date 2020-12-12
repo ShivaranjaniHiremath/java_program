a9e0eeab19ab9eda41c9b3fcde6f2b3dfc362cbd
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:50:42 +0530

    Pattern1

---
+++ PyramidPattern.java
@@ -0,0 +1,18 @@
+
+public class PyramidPattern {
+
+	public static void main(String args[]) {
+		
+		System.out.println("generating pyramid pattern using nested for loop");
+		
+		for (int i=5;i>=0;i--) {
+			
+			for (int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+
+			System.out.println(" ");
+		}
+
+	}
+}
