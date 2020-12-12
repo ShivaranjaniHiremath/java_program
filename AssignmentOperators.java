ad2342877f154b83093b839d346b435473ca390b
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:57:50 +0530

    AssignmentOperators
    using for loop

---
+++ AssignmentOperators.java
@@ -0,0 +1,41 @@
+class AssignmentOperators{
+			public static void main(String args[]){
+				
+				int sum=5;
+				sum=sum+5;
+				System.out.println("the value of sum is:" + sum);
+				sum += 10;
+				System.out.println("the value of sum is:" + sum);
+				
+				int sub=8;
+				sub=sub-2;
+				System.out.println("the value of sub is:" + sub);
+				sub -= 3;
+				System.out.println("the value of sub is:" + sub);
+				
+				int mult=2;
+				mult=mult*2;
+				System.out.println("the value of mult is:" + mult);
+				mult *= 15;
+				System.out.println("the value of mult is:" + mult);
+				
+				int div=105;
+				div=div/5;
+				System.out.println("the value of div is:" + div);
+				div /= 3;
+				System.out.println("the value of div is:" + div);
+				
+				int mod=53;
+				mod=mod/5;
+				System.out.println("the value of mod is:" + mod);
+				mod %= 3;
+				System.out.println("the value of mod is:" + mod);
+			}
+				
+				
+				
+				
+				
+				
+				
+}
