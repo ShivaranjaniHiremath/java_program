fe5ff50541aab74b41013b289362f0988acbfbc8
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 09:00:49 +0530

    LogicalOperations

---
+++ LogicalOperations.java
@@ -0,0 +1,11 @@
+class LogicalOperations {
+
+   public static void main(String args[]) {
+      boolean a = true;
+      boolean b = false;
+
+      System.out.println("result of logical and operation is = " + (a&&b));
+      System.out.println("result of logical or operation is = " + (a||b) );
+      
+   }
+}
