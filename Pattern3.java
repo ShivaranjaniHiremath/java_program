3866fad3e065546a09df832161c50bae393cc41c
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:51:56 +0530

    Pattern3

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+
+public class Pattern3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5,num;
+		for(int i=0;i<n;i++) {
+			num=5;
+			for(int j=0;j<=i;j++) {
+				System.out.print(num+" ");
+				num--;
+			}
+			System.out.println();
+		}
+
+	}
+}
