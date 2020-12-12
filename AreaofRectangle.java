62a73706599f2a327a495ad01682fa139b1bab8e
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:47:08 +0530

    AreaofRectangle

---
+++ AreaofRectangle.java
@@ -0,0 +1,12 @@
+
+public class AreaofRectangle {
+		public static void main(String args[]) {
+			
+			float lenght=3.00f,width=6.50f;
+			
+			float area;
+			area=lenght*width;
+			
+			System.out.println("area of rectangle is:" + area);
+		}
+}
