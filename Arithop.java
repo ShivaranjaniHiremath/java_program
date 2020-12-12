d53db4dfd1bc8064068ef451380b0dcdc8b7a332
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:57:11 +0530

    Arithop
    using for loop

---
+++ Arithop.java
@@ -0,0 +1,22 @@
+class Arithop{
+		public static void main(String args[]){
+		int a=300;
+		int b=20;
+		int c=10;
+		int d=a+b+c;
+		int e=a-b-c;
+		int f=a*b*c;
+		int g=a/b/c;
+		int h=a%b%c;
+		
+		System.out.println("the value of sum of three operands is:" + d );
+		
+		System.out.println("the value of sub of three operands is:" + e );
+		
+		System.out.println("the value of mult of three operands is:" + f );
+		
+		System.out.println("the value of div of three operands is:" + g );
+		
+		System.out.println("the value of mod of three operands is:" + h );
+		}
+}
