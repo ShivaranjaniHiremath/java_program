81adedcfcc07e88de544c155bceaf4d4786c7911
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 09:00:07 +0530

    Incop

---
+++ Incop.java
@@ -0,0 +1,18 @@
+class Incop{
+		public static void main(String args[]){
+		
+			System.out.println("illustrating increament operations");
+			
+			int a=7;
+			int b=4;
+			int preinc=++a;
+			int postinc=b++;
+			
+			
+			System.out.println("the value of preinc is:" + preinc);
+			
+			System.out.println("the value of postinc is:" + postinc);
+			
+		}
+		
+}
