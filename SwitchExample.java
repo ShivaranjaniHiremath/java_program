0117409d6788832d2c8005ab9c081677ff616e42
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:42:49 +0530

    SwitchExample

---
+++ SwitchExample.java
@@ -0,0 +1,14 @@
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("illustration of switch example");
+		System.out.println("end of program");
+		
+		
+
+	}
+
+}
