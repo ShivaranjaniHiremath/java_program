6be9244ae803840edbc7a33243cbc0cfc7381036
ShivaranjaniHiremath <75791683+ShivaranjaniHiremath@users.noreply.github.com>
2020-12-12 08:55:32 +0530

    SumOfNum
    using for loop

---
+++ SumOfNum.java
@@ -0,0 +1,17 @@
+
+public class SumOfNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("program to find some of natural numbers from 5 to 25");
+		
+		int i, sum=0; 
+		
+		for(i=5;i<=25;i++) {
+		sum += i;
+		}
+		System.out.println("sum of natural numbers from 5 to 25 is:" + sum);
+
+	}
+
+}
